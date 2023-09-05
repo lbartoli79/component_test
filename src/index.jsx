c468aeabdc4cd34f023be4226872a63fc55c713f
Leonardo Bartoli <lbartoli@catchpoint.com>
2023-09-05 18:11:37 +0200

    Test component

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Example = () =>
+    <div>
+        <strong>Hello World!!!</strong>
+    </div>
+
+export default Example;
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.jsx',
+  externals: [
+    'react',
+    'react-dom',
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'example.js',
+    library: {
+        name: 'example',
+        type: 'umd'
+    },
+  },
+};
